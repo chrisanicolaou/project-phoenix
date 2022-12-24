c0ea997b5fcaada2bfad795d64e5c03b55f23823
Christos Nicolaou <chris.anicolaou@yahoo.co.uk>
2022-12-24 00:18:19 +0000

    Creating inventory tab in menu

---
+++ Assets/Scripts/UI/GameMenu/Inventory/InventoryPage.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using ChiciStudios.ProjectPhoenix.Items;
+using UnityEngine;
+
+namespace ChiciStudios.ProjectPhoenix.UI.GameMenu.Inventory
+{
+    public class InventoryPage : MonoBehaviour
+    {
+        [SerializeField]
+        private ItemSlot[] _itemSlots;
+
+        [SerializeField]
+        private ItemStore _inventoryStore;
+
+        private int _inventorySize;
+
+        private void OnEnable()
+        {
+            PopulateItemSlots();
+        }
+
+        private void Update()
+        {
+            if (_inventorySize != _inventoryStore.Items.Count) PopulateItemSlots();
+        }
+
+        private void PopulateItemSlots()
+        {
+            _inventorySize = _inventoryStore.Items.Count;
+            for (var i = 0; i < _itemSlots.Length; i++)
+            {
+                if (i >= _inventoryStore.MaxCapacity)
+                {
+                    _itemSlots[i].LockSlot();
+                    continue;
+                }
+                
+                _itemSlots[i].UnlockSlot();
+                if (i < _inventorySize) _itemSlots[i].Populate(_inventoryStore.Items[i]);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GameMenu/Inventory/ItemSlot.cs
@@ -0,0 +1,54 @@
+using System;
+using ChiciStudios.ProjectPhoenix.Items;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace ChiciStudios.ProjectPhoenix.UI.GameMenu.Inventory
+{
+    public class ItemSlot : MonoBehaviour
+    {
+        [SerializeField]
+        private Sprite _lockedSlotSprite;
+        
+        [SerializeField]
+        private Sprite _unoccupiedSlotSprite;
+        
+        [SerializeField]
+        private Sprite _occupiedSlotSprite;
+        
+        [SerializeField]
+        private Image _slotImg;
+        
+        [SerializeField]
+        private Image _itemImg;
+
+        [SerializeField]
+        private TextMeshProUGUI _quantityText;
+        
+        [SerializeField]
+        
+
+        private QuantifiableItem _qItem;
+
+        public void LockSlot()
+        {
+            _slotImg.sprite = _lockedSlotSprite;
+            _quantityText.enabled = false;
+        }
+
+        public void UnlockSlot()
+        {
+            _slotImg.sprite = _unoccupiedSlotSprite;
+            _quantityText.enabled = false;
+        }
+
+        public void Populate(QuantifiableItem qItem)
+        {
+            _qItem = qItem;
+            _slotImg.sprite = _occupiedSlotSprite;
+            _quantityText.enabled = true;
+            _quantityText.text = qItem.Quantity.ToString();
+        }
+    }
+}
